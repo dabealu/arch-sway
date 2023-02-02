cc3fe3f99591f88c2d8687b5ca76a03e5f5fe58a
Dmitry <dabealu@gmail.com>
2023-02-01 19:29:58 -0500

    add flags shortcuts

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,48 @@ use std::env;
 use task_lists::*;
 
 const HELP_MESSAGE: &str = "available flags:
-* list                - print tasks list without execution
-* install             - run tasks to install the system
-* start-from          - start installation from specific task
-* clear-progress      - remove file with saved progress
-* sync                - use git repo to sync configs 
-* qemu                - install and configure qemu/kvm
-* update-bin          - compile new bin from local repo
-* build-iso           - create iso with arch-sway bin included
-* format-dev dev iso  - format device to include storage and boot partitions";
+* l | list                - print tasks list without execution
+* i | install             - run tasks to install the system
+* t | start-from          - start installation from specific task
+* c | clear-progress      - remove file with saved progress
+* s | sync                - use git repo to sync configs 
+* q | qemu                - install and configure qemu/kvm
+* u | update-bin          - compile new bin from local repo
+* b | build-iso           - create iso with arch-sway bin included
+* f | format-dev dev iso  - format device to include storage and boot partitions";
 
 fn main() {
     let mut args = env::args();
     let cmd = args.next().unwrap_or("arch-sway".to_string());
     match args.next() {
         Some(flag) => match flag.as_ref() {
-            "list" => installation_list(parameters::Parameters::dummy()).list(),
-            "install" => installation_list(parameters::Parameters::build()).run(),
-            "sync" => sync_list(parameters::Parameters::build()).run(),
-            "qemu" => qemu_list(parameters::Parameters::build()).run(),
-            "start-from" => {
+            "list" | "l" => installation_list(parameters::Parameters::dummy()).list(),
+            "install" | "i" => installation_list(parameters::Parameters::build()).run(),
+            "sync" | "s" => sync_list(parameters::Parameters::build()).run(),
+            "qemu" | "q" => qemu_list(parameters::Parameters::build()).run(),
+            "start-from" | "t" => {
                 if let Some(task_id) = args.next() {
                     installation_list(parameters::Parameters::build()).run_from(&task_id);
                 } else {
                     println!("usage: {cmd} continue <task_id>");
                 }
             }
-            "clear-progress" => {
+            "clear-progress" | "c" => {
                 if let Err(e) = tasks::clear_progress() {
                     println!("failed to clear current progress: {e}");
                 }
             }
-            "update-bin" => {
+            "update-bin" | "u" => {
                 if let Err(e) = tasks::update_bin() {
                     println!("failed to build new binary: {e}");
                 }
             }
-            "build-iso" => {
+            "build-iso" | "b" => {
                 if let Err(e) = tasks::create_iso() {
                     println!("failed to create iso: {e}");
                 }
             }
-            "format-dev" => {
+            "format-dev" | "f" => {
                 let dev_path = &args.next().expect("error: missing path to storage device");
                 let iso_path = &args.next().expect("error: missing path to iso file");
                 if let Err(e) = tasks::format_device(dev_path, iso_path) {
